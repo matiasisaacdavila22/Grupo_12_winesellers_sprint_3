709d0e2ca41f0ed283d977e68b1803a1c365d22f
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-04-25 23:55:39 -0300

    rompi todo

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require('express');
-const app = express();
-const port= 3001
-const path = require("path");
-const publicPatch = path.resolve(__dirname, "./public");
-app.use(express.static("./public"));
-
-app.get("/", (req, res) => { 
-    res.sendFile(path.join(__dirname, "/views/home.html")) 
-});
-
-app.get("/register", (req, res) => {
-     res.sendFile(path.join(__dirname, "/views/register.html")) 
-    });
-
-app.get("/cart", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/cart.html")) 
-});
-
-app.get("/login", (req, res) => {
-     res.sendFile(path.join(__dirname, "/views/login.html")) 
-    });
-
+c
 app.get("/productDetails", (req, res) => {
      res.sendFile(path.join(__dirname, "/views/productDetails.html")) 
     });
+
+app.use((req, res, next) =>{
+    res.status(404).render('page_404');
+});   
 
 app.listen(port, () => {
     console.log('Listening on port'+port);
